796a4315eb7c024a156cadeb6b040af162f19082
drav28 <drav28@gmail.com>
2014-08-26 00:34:22 +0700

    add disco parallax

---
+++ Assets/scripts/ScrollScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+
+    public float speed = 0;
+
+
+	void Update ()
+    {
+        renderer.material.mainTextureOffset = new Vector2(0f, Time.time * speed);
+	}
+}
